4dc422908ad6bc0b25a3cb48e010d1e05de5a709
just-doit-hana <quanghuy.tran822000@gmail.com>
2021-07-14 10:47:45 +0700

    modified

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,22 @@
-const newRouter = require('./news');
-const siteRouter = require('./site');
+const newRouter = require("./news");
+const siteRouter = require("./site");
 
 // tao ra 1 function
 function route(app) {
-  app.use('/news', newRouter);
-  app.use('/', siteRouter);
+              app.use("/news", newRouter);
+  app.use("/", siteRouter);
 
   // app.get("/", (req, res) => {
   //     res.render("home");
   //   });
 
-  //   app.get("/news", (req, res) => {
+  //app.get("/news", (req, res) => {
   //     res.render("news");
   //   });
 
   // query la 1 doi tuong req.query.q
   // tai vi trong  tk express  cos middleware nen query se lay dc du lieu  co san
-  //   app.get("/search", (req, res) => {
+  // app.get("/search", (req, res) => {
   //     res.render("search");
   //   });
 
